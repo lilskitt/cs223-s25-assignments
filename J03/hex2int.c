440c1c194389d6f0a789f1964121f8fd65727166
Lola Rasmi Peyrouse-Bremer <lpeyrouseb@brynmawr.edu>
2025-03-04 23:04:13 -0500

    lab done

--- J03/hex2int.c
+++ J03/hex2int.c
@@ -1,4 +1,39 @@
-int main()
-{
+//Authors: Lola & Bii
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+  char* input = argv[1];
+  int decimalValue = 0;
+    
+  //check for 0x or 0b
+  if (strncmp(input, "0x", 2) == 0) {
+    //for hex input
+    for (int i = 2; input[i] != '\0'; i++) {
+      //shift left by 4 bits
+      decimalValue = decimalValue << 4;
+      if (input[i] >= '0' && input[i] <= '9') {
+        //for 0-9
+        decimalValue |= (input[i] - '0');
+      } else if (input[i] >= 'A' && input[i] <= 'F') {
+        //for A-f
+        decimalValue |= (input[i] - 'A' + 10);
+      }
+    }
+  } else if (strncmp(input, "0b", 2) == 0) {
+    //for binary input
+    for (int i = 2; input[i] != '\0'; i++) {
+      decimalValue = decimalValue << 1;
+      if (input[i] == '1') {
+        decimalValue = decimalValue | 1;
+      }
+    }
+  } else {
+    return 1;
+  }
+
+  printf("%d\n", decimalValue);
   return 0;
 }
--- J03/int2hex.c
+++ J03/int2hex.c
@@ -1,4 +1,48 @@
-int main()
-{
+//Authors: Lola & Bii
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[]){
+  int num = atoi(argv[1]);
+
+  //hexadecimal
+  printf("0x ");
+  unsigned int mask2 = 0xF0000000;
+  int j = 0;
+  while (mask2 != 0x00000000){
+    if((num & mask2) == 0x00000000){
+      printf("0");
+      j++;
+    } else {
+      unsigned int temp_num = (num & mask2) >> (28-(4*j));
+      printf("%X", temp_num);
+      j++;
+    }
+    mask2 = mask2 >> 4;
+    if(j%2 == 0){
+      printf(" ");
+    }
+  }
+  printf("\n");
+  
+  //binary
+  unsigned int mask = 0b10000000000000000000000000000000;
+  printf("0b ");
+  int i = 0;
+  while (mask != 0b00000000000000000000000000000000){
+    if((num & mask) == 0){
+      printf("0");
+      i++;
+    } else {
+      printf("1");
+      i++;
+    }
+    if(i % 4 == 0){
+      printf(" ");
+    }
+    mask = mask >> 1;
+  }
+  printf("\n");
   return 0;
 }
