bf712313522dec1502df4e84dc39f9bc70aae766
Lola Rasmi Peyrouse-Bremer <lpeyrouseb@brynmawr.edu>
2025-03-04 23:08:02 -0500

    lab done

--- J03/hex2int.c
+++ J03/hex2int.c
@@ -14,14 +14,14 @@ int main(int argc, char *argv[]) {
     for (int i = 2; input[i] != '\0'; i++) {
       //shift left by 4 bits
       decimalValue = decimalValue << 4;
-      if (input[i] >= '0' && input[i] <= '9') {
+      if (input[i] <= '9') {
         //for 0-9
         decimalValue |= (input[i] - '0');
       } else if (input[i] >= 'A' && input[i] <= 'F') {
         //for A-f
         decimalValue |= (input[i] - 'A' + 10);
       }
-    }
+    } //for loop end
   } else if (strncmp(input, "0b", 2) == 0) {
     //for binary input
     for (int i = 2; input[i] != '\0'; i++) {
@@ -29,7 +29,7 @@ int main(int argc, char *argv[]) {
       if (input[i] == '1') {
         decimalValue = decimalValue | 1;
       }
-    }
+    } //for loop end
   } else {
     return 1;
   }
